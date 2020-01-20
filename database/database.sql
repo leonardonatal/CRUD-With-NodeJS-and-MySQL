cd80db6573b9efe6d943fbb70c378d69a6e54965
leonardonatal <36779653+leonardonatal@users.noreply.github.com>
2020-01-20 20:42:24 -0300

    Add files via upload

---
+++ database/database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE ng_games_db;
+
+USE ng_games_db;
+
+CREATE TABLE game(
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(180),
+    desc VARCHAR(255),
+    image VARCHAR(200),
+    image VARCHAR(200),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
